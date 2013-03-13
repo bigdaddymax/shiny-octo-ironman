60dbc1bf9dcc3105e7ac28bc44b9b6fea4f3ba5d
Maksym Bodnar <maksym.bodnar@gmail.com>
2013-03-13 14:45:15 -0700

    Branch supercapex_objects_only

--- application/controllers/FormController.php
+++ application/controllers/FormController.php
@@ -6,33 +6,53 @@
  * @author Olenka
  */
 class FormController extends Zend_Controller_Action {
-    //put your code here
-    
+
+    private $session;
+    private $redirector;
+
+    public function init() {
+        $this->session = new Zend_Session_Namespace('Auth');
+        $this->redirector = $this->_helper->getHelper('Redirector');
+    }
+
     public function indexAction() {
         $objectsManager = new Application_Model_ObjectsManager();
+        $access = new Application_Model_AccessMapper($this->session->userId);
+        $allowedObjects = $access->getAllowedObjectIds();
         $forms = $objectsManager->getAllForms();
-        ($forms === false)?$this->view->forms = 'No forms':$this->view->forms = $forms;
+        ($forms === false) ? $this->view->forms = 'No forms' : $this->view->forms = $forms;
         $dataMapper = new Application_Model_DataMapper();
         $this->view->elements = $dataMapper->getAllObjects('Application_Model_Element');
-        $this->view->orgobjects = $dataMapper->getAllObjects('Application_Model_Orgobject');
+        if (!empty($allowedObjects['write'])) {
+            $this->view->nodes = $dataMapper->getAllObjects('Application_Model_Node', array(0 => array('column' => 'nodeId',
+                    'condition' => 'IN',
+                    'operand' => $allowedObjects['write'])));
+        }
+//               Zend_Debug::dump($this->view->nodes);
     }
-    
-    public function addFormAction(){
+
+    public function addFormAction() {
         $params = $this->getRequest()->getPost();
-        $params['userId'] = 1;
+        $params['userId'] = $this->session->userId;
         $form = new Application_Model_Form($params);
-        if ($form->isValid()){
+        if ($form->isValid()) {
             $objectsManager = new Application_Model_ObjectsManager();
             $this->view->newFormId = $objectsManager->SaveFormData($form);
         } else {
             $this->view->error = 'Cannot create form';
             $this->view->form = $form;
         }
+        $this->redirector->gotoSimple('index', 'form');
     }
-    
-    public function openFormAction(){
-        
+
+    public function openFormAction() {
+        if ($this->_request->isGet()) {
+            $param = $this->getRequest()->getParam('formId');
+            $objectManager = new Application_Model_ObjectsManager();
+            $this->view->form = $objectManager->prepareFormForOutput((int)$param);
+        }
     }
+
 }
 
 ?>
