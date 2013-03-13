8273834c4a744a6d71cb8ddf262c12ef1e930f58
Olenka <Olenka@Olenka-VAIO.san.rr.com>
2013-03-13 14:45:06 -0700

    Branch supercapex_objects_only

--- application/controllers/ObjectsController.php
+++ application/controllers/ObjectsController.php
@@ -12,28 +12,29 @@ class ObjectsController extends Zend_Controller_Action {
     private $objectIdName;
     private $objectName;
     private $subobjects;
+    private $objectsManager;
+    private $session;
 
     public function init() {
         $this->dataMapper = new Application_Model_DataMapper();
+        $this->objectsManager = new Application_Model_ObjectsManager();
         $this->redirector = $this->_helper->getHelper('Redirector');
         switch ($this->_request->getParam('objectType')) {
-            case 'level': $this->className = 'Application_Model_Level';
-                $this->subobjects = array('levels' => $this->dataMapper->getAllObjects('Application_Model_Level'));
+            case 'node': $this->className = 'Application_Model_Node';
+                $this->subobjects = array('nodes' => $this->dataMapper->getAllObjects('Application_Model_Node'));
                 break;
             case 'element': $this->className = 'Application_Model_Element';
                 break;
             case 'position': $this->className = 'Application_Model_Position';
-                $this->subobjects = array('orgobjects' => $this->dataMapper->getAllObjects('Application_Model_Orgobject'));
+                $this->subobjects = array('nodes' => $this->dataMapper->getAllObjects('Application_Model_Node'));
                 break;
             case 'user': $this->className = 'Application_Model_User';
                 $this->subobjects = array('positions' => $this->dataMapper->getAllObjects('Application_Model_Position'));
                 break;
-            case 'orgobject': $this->className = 'Application_Model_Orgobject';
-                $this->subobjects = array('levels' => $this->dataMapper->getAllObjects('Application_Model_Level'));
-                break;
-            default : $this->className = 'Application_Model_Level';
+            default : $this->className = 'Application_Model_Node';
         }
         $this->setClassAndTableName($this->className);
+        $this->session = new Zend_Session_Namespace('Auth');
     }
 
     private function setClassAndTableName() {
@@ -50,7 +51,16 @@ class ObjectsController extends Zend_Controller_Action {
     }
 
     public function addObjectAction() {
+        function stripUnderscore(&$value, $key){
+            $value = ltrim($value, '_');
+        }
         $params = $this->getRequest()->getPost();
+        $keys = array_keys($params);
+        $values = array_values($params);
+ //       $keys = array_keys($params);
+        array_walk($keys, 'stripUnderscore');
+        $params = array_combine($keys, $values);
+        Zend_Debug::dump($params);
         $object = new $this->className($params);
         if ($object->isValid()) {
             $this->dataMapper->saveObject($object);
@@ -73,15 +83,31 @@ class ObjectsController extends Zend_Controller_Action {
             $this->view->exceptionMessage = 'Got exception while trying to delete ' . $this->objectName . ': ' . $e->getMessage();
         }
     }
-    
-    public function editPrivilegesAction(){
+
+    public function userPrivilegesAction() {
         $userId = (int) $this->_request->getParam('userId');
-        if ($userId && $this->_request->isPost){
-            $this->view->privileges = $this->dataMapper->getAllObjects('Application_Model_Privilege');
+        if ($userId /* && $this->_request->isPost */) {
             $this->view->user = $this->dataMapper->getObject($userId, 'Application_Model_User');
+            $this->view->userPrivileges = $this->objectsManager->getPrivilegesTable($userId);
         }
     }
-    
+
+    public function editPrivilegesAction() {
+        if ($this->_request->isPost()) {
+            $privilege = new Application_Model_Privilege(array('userId' => $this->_request->getParam('userId'),
+                        'objectType' => $this->_request->getParam('object'),
+                        'objectId' => $this->_request->getParam('objectId'),
+                        'privilege' => $this->_request->getParam('privilege'),
+                        'domainId' => 1));
+            if ((bool)$this->_request->getParam('state')) {
+                $this->objectsManager->grantPrivilege($privilege);
+            } else {
+                $this->objectsManager->revokePrivilege($privilege);
+            }
+        }
+        exit;
+    }
+
 }
 
 ?>
