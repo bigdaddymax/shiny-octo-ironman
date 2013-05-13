eb52cf8f3413dc2fb0368035b9fcf90be054b4fb
Maksym Bodnar <maksym.bodnar@gmail.com>
2013-05-13 12:19:56 +0300

    Commit before translation

---
+++ tests/application/models/CommentTest.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Description of CommentTest
+ *
+ * @author Max
+ */
+require_once TESTS_PATH . '/application/TestCase.php';
+
+class CommentTest extends TestCase {
+
+    public function testCommentGetterSetter() {
+        $comment = new Application_Model_Comment();
+        $comment->text = 'test test test test';
+        $this->expectOutputString('Cant set value. Property commentStatus doesnt exist');
+        $comment->commentStatus = 'status';
+        ob_clean();
+        $test = $comment->commentState;
+        $this->assertEquals('Cannot get value. Property commentState doesnt exist', $test);
+        $this->expectOutputString('Cannot set value for "valid" property');
+        $comment->valid = 1;
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testCommentConstructException() {
+        $comment = new Application_Model_Comment(1);
+    }
+
+    public function testCommentConstructCorrect() {
+        $commentArray = array('comment' => 'eName', 'parentCommentId'=>-1,'commentId' => 3, 'active' => 0, 'userId'=>3, 'formId'=>2, 'domainId' => 5, 'date'=>date('%d-%m-%Y'));
+        $comment = new Application_Model_Comment($commentArray);
+        $this->assertTrue($comment->isValid());
+        $commentArray1 = $comment->toArray();
+        unset($commentArray['date']);
+        unset($commentArray1['date']);
+        $this->assertEquals($commentArray, $commentArray1);
+    }
+
+    public function testCommentValidation() {
+        $commentArray = array('commentName' => 'pName');
+        $comment = new Application_Model_Comment($commentArray);
+        $this->assertFalse($comment->isValid());
+        $comment->comment = 'eName';
+        $this->assertFalse($comment->isValid());
+        $comment->domainId = 3;
+        $this->assertFalse($comment->isValid());
+        $comment->userId = 3;
+        $this->assertFalse($comment->isValid());
+        $comment->date = date('%m%d%Y');
+        $this->assertFalse($comment->isValid());
+        $comment->commentId = 1;
+        $this->assertFalse($comment->isValid());
+        $comment->parentCommentId = -1;
+        $this->assertFalse($comment->isValid());
+        $comment->formId = 1;
+        $this->assertTrue($comment->isValid());
+        $this->assertEquals('eName', $comment->comment);
+        $this->assertEquals(1, $comment->commentId);
+    }
+
+
+
+    
+    public function testCommentToArray() {
+        $commentArray = array('comment' => 'eName', 'commentId' => 3,'active' => 0, 'domainId' => 5);
+        $comment = new Application_Model_Comment($commentArray);
+        $commentArray2 = $comment->toArray();
+         $this->assertEquals($commentArray, $commentArray2);
+    }
+    
+    public function testSaveComment(){
+        $objectManager = new Application_Model_ObjectsManager(5);
+        $commentArray = array('comment' => 'eName', 'parentCommentId'=>-1,'commentId' => 3, 'active' => 0, 'userId'=>3, 'formId'=>2, 'domainId' => 5, 'date'=>date('%d-%m-%Y'));
+        $comment = new Application_Model_Comment($commentArray);
+        $this->assertTrue($comment instanceof Application_Model_Comment);
+        $this->assertTrue($comment->isValid());
+        $id = $objectManager->saveObject($comment);
+        $this->assertTrue(is_int($id));
+    }
+}
