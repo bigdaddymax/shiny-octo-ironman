67a8175257c7a75535dc697e5b01caba51ac401f
Maksym Bodnar <maksym.bodnar@gmail.com>
2013-05-13 12:21:09 +0300

    Another commit

---
+++ library/Capex/lang/en/translation.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+    'send'      =>'Send',
+    'publish'   =>'Publish',
+    'add form'  =>'Add Form',
+    'edit form' =>'Edit Form',
+    'save form' =>'Save From',
+    'add item'  =>'Add Item',
+    'login'     =>'Login',
+    'logout'    =>'Logout',
+    'signup'    =>'Sign Up',
+    'email'     =>'email',
+    'name'      =>'name',
+    'approve'   =>'Approve',
+    'decline'   =>'Decline',
+    'total'     =>'Total',
+    'form name' =>'Form Name'
+);
